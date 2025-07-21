551397149a3013893790114b1518d137fd0a3714
Jeremy Gunawan <jemygunawanliu@gmail.com>
2025-07-21 21:42:03 +0700

    new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   Birth Verification/Birth Verification.cpp 	new file:   Birth Verification/Birth Verification.exe

---
+++ Birth Verification/Birth Verification.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+    int tahun;
+    int bulan;
+    int hari;
+
+    /*
+    Birth Verification
+    ===============
+    Give Your Born date, we give your age
+    */
+
+    cout << "====BIRTH VERIFICATION====" << endl;
+    cout << endl;
+    cout << "Enter Your Birth Year(YYYY/MM/DD): ";
+    cin >> tahun >> bulan >> hari;
+    cout << bulan << "/" << hari << "/" << tahun << endl;
+    
+    
+
+
+
+
+
+    return 0;
+}
