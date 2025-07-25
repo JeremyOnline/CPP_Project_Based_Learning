0593ea62fe23cd7318d0a887d92865458f3dd112
Jeremy Gunawan <jemygunawanliu@gmail.com>
2025-07-26 04:38:30 +0700

    modified:   .vscode/launch.json 	modified:   Birth Verification/Birth Verification.cpp 	modified:   Birth Verification/Birth Verification.exe

--- Birth Verification/Birth Verification.cpp
+++ Birth Verification/Birth Verification.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <ctime>
+
 
 
 using namespace std;
+
+int umur_user(int yyyy, int mm, int dd){
+    int tahun = 2025 - yyyy;
+    int bulan = mm - 7;
+    int hari = dd - 26;
+    int umur = tahun + bulan/12 + hari/365;
+    return umur;
+}
 
 
 int main() {
@@ -19,6 +29,7 @@ int main() {
     cout << endl;
     cout << "Enter Your Birth Year(YYYY/MM/DD): ";
     cin >> tahun >> bulan >> hari;
+    cout <<"Umur Kamu adalah : " << umur_user(tahun, bulan, hari) << " tahun" << endl;
     cout << bulan << "/" << hari << "/" << tahun << endl;
     
     
