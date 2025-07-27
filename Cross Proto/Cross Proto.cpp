099866f7df6225c3c1decfa8778661df8e2be24f
Jeremy Gunawan <jemygunawanliu@gmail.com>
2025-07-27 21:15:34 +0700

    modified:   Cross Proto/Cross Proto.cpp 	modified:   Cross Proto/Cross Proto.exe

--- Cross Proto/Cross Proto.cpp
+++ Cross Proto/Cross Proto.cpp
@@ -7,9 +7,12 @@ int main(){
     cout << "Enter the number of elements: ";
     cin >> input;
     int space;
+    int space_2;
     space = input;
+    space_2 = 2 * input;
     cout << "Spasi : " << &space << endl;
     cout << "input : " << &input << endl;
+    cout << "Spasi 2 : " << &space_2 << endl;
     
     
     /*
@@ -27,12 +30,20 @@ int main(){
     */
 
     for (int height = 1; height <= 4*input; height++){
-        for(int spasi_1 = 1; spasi_1 <= space; spasi_1++){
-            cout << " ";
+        if (height <= space)
+        {
+            for(int spasi_1 = 1; spasi_1 <= space; spasi_1++){
+                cout << " ";
+            }
+            for(int column = 1; column <= space; column++){
+                cout << "*";
+            }
         }
-        for(int column = 1; column <= space; column++){
-            cout << "*";
-        }
+        // if(height <= space_2){
+        //     for(int spasi_2 = 1; spasi_2 <= space; spasi_2++){
+        //         cout << "*";
+        //     }
+        // }
         cout << endl;  
     }
     
